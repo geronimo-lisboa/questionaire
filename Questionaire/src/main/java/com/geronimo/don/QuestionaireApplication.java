e0089a035c351b8b24f8cafbad4d26f4d6cf02ad
geronimo-lisboa <erikaeluciano2016@gmail.com>
2017-12-28 22:32:34 -0200

    Primeiro commit

---
+++ Questionaire/src/main/java/com/geronimo/don/QuestionaireApplication.java
@@ -0,0 +1,12 @@
+package com.geronimo.don;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionaireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionaireApplication.class, args);
+	}
+}
